ba60f75667eaec4595f222d541721588246f08be
BrendorTheEndor <brenden_hoverxd@live.com>
2019-08-13 13:18:13 -0500

    Right before movement

---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicPlayer : MonoBehaviour {
+
+    [SerializeField] float timeToDelayLoad = 2f;
+
+    private void Awake() {
+        int numberOfObject = FindObjectsOfType<MusicPlayer>().Length;
+        if(numberOfObject > 1) {
+            Destroy(gameObject);
+        }
+        else {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start() {
+        StartCoroutine(WaitAndLoadNextScene());
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    IEnumerator WaitAndLoadNextScene() {
+        yield return new WaitForSeconds(timeToDelayLoad);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
